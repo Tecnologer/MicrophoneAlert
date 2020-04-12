f9b2802e80178318e8a920d52187ac110c149aa8
tecnologer <rdominguez@tecnologer.net>
2020-04-11 21:49:14 -0500

    initial commit

---
+++ MicrophoneAlert.net/AudioDevices.cs
@@ -0,0 +1,70 @@
+﻿using NAudio.CoreAudioApi;
+using System.Diagnostics;
+
+namespace MicrophoneAlert.net
+{
+    public class AudioDevices
+    {
+        #region Singleton
+        private static readonly object _syncRoot = new object();
+        private static AudioDevices instance;
+        public static AudioDevices Instance
+        {
+            get
+            {
+                lock (_syncRoot)
+                {
+                    return instance ?? (instance = new AudioDevices());
+                }
+            }
+        }
+        #endregion
+
+        private MMDeviceCollection mMDevices;
+        private string selectedDeviceId;
+        public AudioDevices()
+        {
+            SelectedDevice = null;
+            SelectedDeviceId = "{0.0.1.00000000}.{e08360f0-1cb0-4e44-87be-89fc924d260e}";
+        }
+
+        public MMDeviceCollection Devices { get => mMDevices; }
+
+        public string SelectedDeviceId
+        {
+            get
+            {
+                return selectedDeviceId;
+            }
+            set
+            {
+                if (selectedDeviceId == value) return;
+                selectedDeviceId = value;
+                UpdateListDevices();
+            }
+        }
+
+        public MMDevice SelectedDevice { get; private set; }
+
+        public void UpdateListDevices()
+        {
+            var enumerator = new MMDeviceEnumerator();            
+            mMDevices = enumerator.EnumerateAudioEndPoints(DataFlow.Capture, DeviceState.Active);
+            SelectedDevice = enumerator.GetDevice(SelectedDeviceId);
+            foreach (var wasapi in mMDevices)
+            {
+                Debug.WriteLine($"{wasapi.ID}:{wasapi.DataFlow} {wasapi.FriendlyName} {wasapi.DeviceFriendlyName} {wasapi.State}");
+            }
+        }
+
+        public float GetVolume()
+        {
+            if(SelectedDevice == null || SelectedDevice.ID != SelectedDeviceId)
+            {
+                UpdateListDevices();
+            }
+            SelectedDevice.AudioEndpointVolume.Mute = false;
+            return SelectedDevice != null ? SelectedDevice.AudioMeterInformation.MasterPeakValue * 100 : 0;
+        }
+    }
+}
---
+++ MicrophoneAlert.net/MainWindow.xaml.cs
@@ -0,0 +1,91 @@
+﻿using System.ComponentModel;
+using System.Timers;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace MicrophoneAlert.net
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : INotifyPropertyChanged
+    {
+        private string decibelsValue;
+        private SolidColorBrush backgroundColor;
+
+        private Timer timer;
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = this;
+
+            timer = new Timer(500);
+            timer.Elapsed += Timer_Elapsed;
+            timer.Enabled = true;
+        }
+
+        public SolidColorBrush BackgroundColor
+        {
+            get
+            {
+                return backgroundColor;
+            }
+            set
+            {
+                if (backgroundColor?.Color == value?.Color) return;
+                backgroundColor = value;
+                OnPropertyChanged(nameof(BackgroundColor));
+            }
+        }
+
+        public string DecibelsValue
+        {
+            get
+            {
+                return decibelsValue;
+            }
+            set
+            {
+                if (decibelsValue == value) return;
+                decibelsValue = value;
+                OnPropertyChanged(nameof(DecibelsValue));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(params string[] propertiesName)
+        {
+            foreach (var propertyName in propertiesName)
+            {
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        private void Timer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            try
+            {
+                timer.Enabled = false;
+                var vol = AudioDevices.Instance.GetVolume();
+
+                Dispatcher.Invoke(delegate
+                {
+                    DecibelsValue = ((int)vol).ToString();
+                    var color = vol >= 70 ? Colors.Red : Colors.Lime;
+                    BackgroundColor = new SolidColorBrush(color);
+                });
+            }
+            finally
+            {
+                timer.Enabled = true;
+            }
+        }
+
+        private void Grid_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+                DragMove();
+        }
+    }
+}

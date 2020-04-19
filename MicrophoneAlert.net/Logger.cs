2b30c72ee031b59452f584318e1b9285ccc0b2c4
tecnologer <rdominguez@tecnologer.net>
2020-04-19 15:55:33 -0500

    Create logger

---
+++ MicrophoneAlert.net/Logger.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+namespace MicrophoneAlert.net
+{
+    internal class Logger
+    {
+        private const string path = "./logger.log";
+        internal void Error(Exception ex, string v)
+        {
+            if (!File.Exists(path))
+            {
+                // Create a file to write to.
+                using (StreamWriter sw = File.CreateText(path))
+                {
+                    sw.WriteLine($"{DateTime.Now} \tFirst start");
+                }
+            }
+
+            using (StreamWriter sw = File.AppendText(path))
+            {
+                sw.WriteLine($"\n{DateTime.Now} \t{v}");
+                sw.WriteLine($"\n{DateTime.Now} \t{ex.Message}");
+                sw.WriteLine($"\n{DateTime.Now} \t{ex.StackTrace}");
+            }
+        }
+    }
+}

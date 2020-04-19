5a6f7ae7011b72a99df35a833dbe5aa5e477e5cb
tecnologer <rdominguez@tecnologer.net>
2020-04-19 15:56:17 -0500

    Implement logger in App

--- MicrophoneAlert.net/App.xaml.cs
+++ MicrophoneAlert.net/App.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using MahApps.Metro;
 using System;
+using System.Threading.Tasks;
 using System.Windows;
 
 namespace MicrophoneAlert.net
@@ -9,6 +10,7 @@ namespace MicrophoneAlert.net
     /// </summary>
     public partial class App : Application
     {
+        private static Logger _logger = new Logger();
         protected override void OnStartup(StartupEventArgs e)
         {
             // get the current app style (theme and accent) from the application
@@ -21,6 +23,43 @@ namespace MicrophoneAlert.net
                                         ThemeManager.GetAppTheme("BaseLight")); // or appStyle.Item1
 
             base.OnStartup(e);
+            SetupExceptionHandling();
+        }
+
+        private void SetupExceptionHandling()
+        {
+            AppDomain.CurrentDomain.UnhandledException += (s, e) =>
+                LogUnhandledException((Exception)e.ExceptionObject, "AppDomain.CurrentDomain.UnhandledException");
+
+            DispatcherUnhandledException += (s, e) =>
+            {
+                LogUnhandledException(e.Exception, "Application.Current.DispatcherUnhandledException");
+                e.Handled = true;
+            };
+
+            TaskScheduler.UnobservedTaskException += (s, e) =>
+            {
+                LogUnhandledException(e.Exception, "TaskScheduler.UnobservedTaskException");
+                e.SetObserved();
+            };
+        }
+
+        private void LogUnhandledException(Exception exception, string source)
+        {
+            string message = $"Unhandled exception ({source})";
+            try
+            {
+                System.Reflection.AssemblyName assemblyName = System.Reflection.Assembly.GetExecutingAssembly().GetName();
+                message = string.Format("Unhandled exception in {0} v{1}", assemblyName.Name, assemblyName.Version);
+            }
+            catch (Exception ex)
+            {
+                _logger.Error(ex, "Exception in LogUnhandledException");
+            }
+            finally
+            {
+                _logger.Error(exception, message);
+            }
         }
     }
 }
